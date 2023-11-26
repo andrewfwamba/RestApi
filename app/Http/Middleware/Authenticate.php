99c90710a16b9c72582ddbc102d8401dd3632cb8
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-11-26 16:58:28 +0300

    task: handle token validity

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,13 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request)
     {
-        return $request->expectsJson() ? null : route('login');
+        // if ($request->expectsJson()) {
+        //     throw new AuthenticationException('Unauthenticated.', [response()->json(['success' => false, 'message' => 'Invalid token'], 401)]);
+        // }
+
+        // return null;
+        return $request->expectsJson() ? null : response()->json(['success' => false, "message" => "Invalid token"], 401);
     }
 }
