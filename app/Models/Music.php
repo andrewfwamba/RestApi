7bc42eaaa377d3038cb64d20f94e598e80681474
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-11-26 16:55:08 +0300

    task: eloquence relationship setup

--- app/Models/Music.php
+++ app/Models/Music.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Music extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'author', 'url', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
